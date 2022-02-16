870a53f441816c1f8fc9f6553eee08c768ed608d
Abdelmasieh01 <gokuabdo31@gmail.com>
2022-02-16 04:52:42 +0200

    Revert "First version of the app into github"
    This reverts commit 931e0830234768c5f025baf8de7c4b22317b7f16.

--- logreg/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-admin.site.site_header='My Secret Hater Database'
-admin.site.site_title='Adminstration'
-admin.site.index_title='My Secret Hater'
