191688376bc1f1eabd1f3f64ad7a6092773589b4
Abdelmasieh01 <gokuabdo31@gmail.com>
2022-02-17 04:09:39 +0200

    Update settings.py

--- mysecrethater/settings.py
+++ mysecrethater/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
-DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'mysecrethater.herokuapp.com'
