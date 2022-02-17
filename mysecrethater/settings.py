d3a03a482189217b44c2ba40320c897e07879793
Abdelmasieh01 <gokuabdo31@gmail.com>
2022-02-17 02:32:20 +0200

    Update settings.py

--- mysecrethater/settings.py
+++ mysecrethater/settings.py
@@ -22,13 +22,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = config('')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'mysecrethater.herokuapp.com',
+]
 
 
 # Application definition
