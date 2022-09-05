d1fa3d296cdbf7e9062865f89d3ebb3b07ca4e3f
Abdelmasieh01 <gokuabdo31@gmail.com>
2022-09-05 17:51:52 +0200

    test.

--- mysecrethater/settings.py
+++ mysecrethater/settings.py
@@ -115,9 +115,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
 }
 
 
