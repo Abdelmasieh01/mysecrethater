74f38c27acf32373e3fe34d59fc64eb644eeae4f
Abdelmasieh01 <gokuabdo31@gmail.com>
2022-09-05 23:37:23 +0200

    added api auth token

--- mysecrethater/urls.py
+++ mysecrethater/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 from api import views
 from rest_framework import routers
-from rest_framework.authtoken import views
+from rest_framework.authtoken import views as _views
 
 router = routers.DefaultRouter()
 router.register(r'messages', views.MessageViewset)
@@ -16,5 +16,5 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path('api-overview/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api-token-auth/', views.obtain_auth_token, name='auth-token'),
+    path('api-token-auth/', _views.obtain_auth_token, name='auth-token'),
 ]
